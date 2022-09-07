0fec4d35ed6ee1009a28df27c9e579ec7beffce2
Alisher <info@regname.uz>
2022-09-07 13:49:48 +0500

    verstka

---
+++ marketing/js/main.js
@@ -0,0 +1,10 @@
+$(function() {
+  $(".slider-blog__inner").slick({
+    slidesToShow: 1,
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+  });
+})
